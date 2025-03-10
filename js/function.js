45a18259cf95776cf7e8e86b3e85e3154dbf9888
SH-U-VO <mdshuvo16122003@gmail.com>
2025-03-10 07:35:21 +0600

    jibon sesh

--- js/function.js
+++ js/function.js
@@ -1,12 +1,18 @@
-function processDonation(donateNowId, donateAmountId, cardDonateAmountId, myAccountID) {
+function processDonation(donateNowId, donateAmountId, cardDonateAmountId, myAccountID, myModalID, donationTitle) {
     const donateNow = document.getElementById(donateNowId);
-    const myAccountText = document.getElementById(myAccountID).innerText;
-    const myAccount = parseFloat(myAccountText.replace(/[^0-9.]/g, ''), 10);
+    
+    const modal = document.getElementById(myModalID);
 
     donateNow.addEventListener('click', function () {
+        const myAccountText = document.getElementById(myAccountID).innerText;
+    const myAccount = parseFloat(myAccountText.replace(/[^0-9.]/g, ''), 10); 
+
         const donateAmountText = document.getElementById(donateAmountId).value;
         const donateAmount = parseFloat(donateAmountText);
         console.log(donateAmount);
+
+        // Donation history
+        addDonationHistory(donateAmount, donationTitle);
 
         if (myAccount < donateAmount) {
             alert("Earn Money First");
@@ -19,7 +25,7 @@ function processDonation(donateNowId, donateAmountId, cardDonateAmountId, myAcco
         }
 
         const cardDonateAmountText = document.getElementById(cardDonateAmountId).textContent;
-        const cardDonateAmount = parseFloat(cardDonateAmountText.replace(/[^0-9.]/g, ''));
+        const cardDonateAmount = parseFloat(cardDonateAmountText.replace(/[^0-9.]/g, ''), 10);
         console.log(cardDonateAmount);
 
         if (!isNaN(donateAmount) && !isNaN(cardDonateAmount)) {
@@ -29,13 +35,37 @@ function processDonation(donateNowId, donateAmountId, cardDonateAmountId, myAcco
             document.getElementById(cardDonateAmountId).textContent = cardTotalDonateAmount + " BDT";
 
             document.getElementById(donateAmountId).value = '';
+            success = true;
 
         } else {
             alert("Invalid input: Please enter a valid Number");
             document.getElementById(donateAmountId).value = '';
+            success = false;
         }
+
+        // modal
         const returnAmount = myAccount - donateAmount;
         document.getElementById(myAccountID).innerText = returnAmount + " BDT";
 
+        if (success) {
+            modal.showModal();
+        }
+
     });
+}
+
+function addDonationHistory(amount, title) {
+    const historyContainer = document.getElementById('donationHistory');
+    const date = new Date();
+
+    const historyItem = `
+    <div tabindex="0" class="collapse collapse-open bg-base-100 border-base-300 border my-3">
+        <div class="collapse-title font-semibold">${amount} Taka is Donated for ${title}</div>
+        <div class="collapse-content text-sm">
+            Date : ${date.toString()}
+        </div>
+    </div>
+`;
+
+    historyContainer.innerHTML += historyItem;
 }
--- js/home.js
+++ js/home.js
@@ -1,7 +1,30 @@
-processDonation('donate-now-1', 'donate-amount-1', 'card-donate-amount-1', 'my-account');
-processDonation('donate-now-2', 'donate-amount-2', 'card-donate-amount-2', 'my-account');
-processDonation('donate-now-3', 'donate-amount-3', 'card-donate-amount-3', 'my-account');
+console.log('hlw');
+processDonation('donate-now-1', 'donate-amount-1', 'card-donate-amount-1', 'my-account', 'my-modal', 'Flood Relief in Feni, Bangladesh');
 
 
+ processDonation('donate-now-2', 'donate-amount-2', 'card-donate-amount-2', 'my-account', 'my-modal', 'Flood Relief in Feni, Bangladesh');
 
+processDonation('donate-now-3', 'donate-amount-3', 'card-donate-amount-3', 'my-account', 'my-modal', 'Aid for Injured in the Quota Movement, Bangladesh');
+
+const btn1 = document.getElementById('btn-1');
+const btn2 = document.getElementById('btn-2');
+const main = document.getElementById('main');
+const mainHTML = main.innerHTML;
+const donationHistory = document.getElementById('donationHistory');
+
+btn1.addEventListener('click', function() {
+    btn1.style.backgroundColor = '#B4F461';
+    btn2.style.backgroundColor = ''; 
+    main.innerHTML = mainHTML;
+    donationHistory.classList.add('hidden');
+
+});
+
+btn2.addEventListener('click', function() {
+    btn2.style.backgroundColor = '#B4F461';
+    btn1.style.backgroundColor = ''; 
+
+    main.innerHTML = '';
+    donationHistory.classList.remove('hidden');
+});
 
